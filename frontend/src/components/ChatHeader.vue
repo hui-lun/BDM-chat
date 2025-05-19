f4e63780dc5093214de20fe478b638740a2442f1
chiaoooo <a0958890729@gmail.com>
2025-05-19 14:27:30 +0800

    change url

--- frontend/src/components/ChatHeader.vue
+++ frontend/src/components/ChatHeader.vue
@@ -8,7 +8,7 @@
       <span class="icon" @click="$emit('open-history-menu')" title="聊天歷史紀錄">🕑</span>
       <!-- <span class="icon" @click="$emit('clear-messages')" title="清除聊天紀錄">🗑️</span> -->
       <span class="icon" @click="$emit('send-email-content')" title="傳入信件內容">✉️</span>
-      <a class="icon" href="https://d6e7-59-124-2-11.ngrok-free.app/" target="_blank" title="連網址">🌐</a>
+      <a class="icon" href="https://96fc-59-124-2-11.ngrok-free.app/" target="_blank" title="連網址">🌐</a>
     </div>
   </div>
 </template>
